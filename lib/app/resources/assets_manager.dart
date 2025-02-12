90e1cdf63da8311f54981498dd3825f992968442
NguyenT <nguyenthe171052@fpt.edu.vn>
2025-02-12 09:15:55 +0700

    assets manage add

---
+++ lib/app/resources/assets_manager.dart
@@ -0,0 +1,15 @@
+class ImageAssets {
+  static String baseImage = 'assets/images';
+
+  static String image1 =  '$baseImage/images';
+  static String welcomeBoard3 = '$baseImage/ridemate_logo.png';
+  static String welcome1 = '$baseImage/welcome_1.png';
+  static String welcome2 = '$baseImage/welcome_2.png';
+  static String welcome3 = '$baseImage/welcome_3.png';
+
+}
+
+class IconAssets {
+  static String baseIcon = 'assets/icons';
+
+}
