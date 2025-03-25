c2ed34bd133c59289b809fb729bd0b0267122261
nhat <vahidnguyen@gmail.com>
2025-03-25 10:17:20 +0700

    commit

--- lib/app/base/api_base_url.dart
+++ lib/app/base/api_base_url.dart
@@ -2,5 +2,5 @@ class ApiBaseUrl {
   // static const String baseUrl = "http://10.0.2.2:8080";  //may ao
   // static const String baseUrl = "http://localhost:8080"; //local
   // static const String baseUrl = "http://192.168.128.215:8080"; //home
-  static const String baseUrl = "http://192.168.1.11:8080"; //fsoft
+  static const String baseUrl = "http://192.168.129.3:8080"; //fsoft
 }
